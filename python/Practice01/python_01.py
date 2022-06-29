a8e0b643930c70ec977c624aaa87186b4d2d7962
Jongkwan Choi <doemqjffns97@naver.com>
2022-06-29 23:47:05 +0900

    first commit

---
+++ python/Practice01/python_01.py
@@ -0,0 +1,17 @@
+from unicodedata import name
+
+
+print(12345)
+print('hi my frist python' )
+print(5>4)
+print(not True)
+print(not 5>10)
+name = "초코"
+animal = "강아지"
+age = 2
+hobby = "간식"
+is_adult = age>= 3
+
+print("우리집"+ animal + "의 이름은" + name + "입니다")
+print(name + "는" + str(age) + "살이고" + hobby + "를 매우 좋아합니다")
+print(name + "는 어른일까요? " + str(is_adult))
